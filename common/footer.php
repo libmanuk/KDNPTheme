2cb9c168926cc71002ed30c89097523f9a0ced39
Eric C. Weig <eweig@email.uky.edu>
2017-02-03 15:56:05 -0500

    Add files via upload

---
+++ common/footer.php
@@ -0,0 +1,56 @@
+		<footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<span><img src="https://kentuckynewspapers.org/kdnp/themes/kdnp/UKlogo-white_sm.png" alt="UK Logo"/></span>
+
+
+
+				<p class="footer-company-name">Kentucky Digital Newspaper Program &copy; 2016<br/>Utilizing <a href="http://omeka.org/" target="_blank">Omeka</a>, <a href="https://lucene.apache.org/solr/" target="_blank">Solr</a>, and the <a href="https://archive.org" target=_blank">Internet Archive</a>.</p>
+
+
+				<div class="footer-icons">
+
+					<a href="https://www.facebook.com/KyDNP/" target="_blank"><i class="fa fa-facebook"></i></a><br/>
+                                        <div id="fboverlay" class="fb-like" data-href="https://www.facebook.com/KyDNP/" data-layout="box_count" data-action="like" data-size="small" data-show-faces="true" data-share="false" style="float:left;margin-right:10px;margin-top:10px;"></div><br/>
+					<!--<a href="#"><i class="fa fa-twitter"></i></a>-->
+					<!--<a href="#"><i class="fa fa-github"></i></a>-->
+
+				</div>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span><a href="https://libraries.uky.edu/lib.php?lib_id=13" target="_blank">Special Collections Research Center</a><br/><a href="https://libraries.uky.edu" target="_blank">University of Kentucky Libraries</a><br/>Margaret I. King Library Building<br/>Lexington, KY 40506-0039</span></p>
+				</div>
+
+				<!--<div>
+					<i class="fa fa-phone"></i>
+					<p>+1 859 257-1742</p>
+				</div>-->
+
+				<!--<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:SCLREF@LSV.UKY.EDU">SCLREF@LSV.UKY.EDU</a></p>
+				</div>-->
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span><a href="https://kdnp.uky.edu/project" target="_blank"><img src="https://kentuckynewspapers.org/kdnp/themes/kdnp/newsboy_footer.png" alt="KDNP Logo"/></a></span>
+					<a href="https://kdnp.uky.edu/project" target="_blank">visit our website to learn more about our program</a>
+				</p>
+
+
+
+			</div>
+
+		</footer>
+
+	</body>
+
+</html>
